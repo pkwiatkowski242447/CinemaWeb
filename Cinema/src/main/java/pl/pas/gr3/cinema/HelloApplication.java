27424132660e6fc418a9cc4ead770293c4838f84
242447 Kwiatkowski Piotr <242447@edu.p.lodz.pl>
2023-11-05 13:52:07 +0100

    Initial commit. Creation of the project and configuration of plugins.

---
+++ Cinema/src/main/java/pl/pas/gr3/cinema/HelloApplication.java
@@ -0,0 +1,9 @@
+package pl.pas.gr3.cinema;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
